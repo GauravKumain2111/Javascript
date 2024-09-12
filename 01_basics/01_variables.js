43a58e31ac4462d2973bbf4ed484009a51193844
GauravKumain2111 <122596381+GauravKumain2111@users.noreply.github.com>
2024-09-12 15:01:47 +0000

    datatypes & variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 14423
+let accountEmail = "gaurav12@gmail.com"
+var password = "gaurav"
+accountCity = "Punjab"
+
+//let's try to change everything and see the behaviour
+
+// accountId = 111 //tried cannot change
+// console.log(accountId); 
+
+accountEmail = "gaurav11@gmail.com"
+
+password = "goku"
+
+
+accountCity="Rajpura"
+
+console.table([accountId,accountEmail,password,accountCity])
+
+/*
+
+var used for global scope in old times 
+but now let is mostly used
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all JS code as newer version of js
+
+// alert(3+3) we are using nodejs not browser so cannot work for now
+
+
+// datatypes
+
+// number=> 2 to power 53
+//bigInt
+//string=>" "
+//boolean => true or false
+//null =>standalone value/empty value
+//undefined =>value not defined but variable is defined
+//symbol =>unique
+
+//Object
+
+console.log(typeof "gaurav");
+console.log(typeof null); //output is object
+console.log(typeof undefined);//output is undefined
+
+
+
