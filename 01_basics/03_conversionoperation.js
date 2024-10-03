4d1825488a057587dfda0782decaa1fca97bbe76
GauravKumain2111 <122596381+GauravKumain2111@users.noreply.github.com>
2024-10-03 10:09:11 +0000

    COnversion of Datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,10 @@
+let score = "88abc";
+
+console.log(typeof(score));
+
+let valueinscore = Number(score);
+console.log(valueinscore);
+console.log(typeof(valueinscore));
+
+// "33"=>String
+// "3333abc"=>NaN (Not a number)
